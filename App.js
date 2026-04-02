63d976d94669df2c41fc040b2e7798775b3842fc
srjuninn <ryanferreiradasilva2006@gmail.com>
2026-04-02 11:03:55 -0300

    feat: add todolist mobile simple version

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>O neymar tem que estar na seleção</Text>
       <StatusBar style="auto" />
     </View>
   );
