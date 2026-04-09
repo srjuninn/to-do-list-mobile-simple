7bb4d01d51cf7becbf28d54e801a5f0496de7140
srjuninn <ryanferreiradasilva2006@gmail.com>
2026-04-09 11:05:30 -0300

    feat: add task modal

---
+++ src/components/TaskModal/index.js
@@ -0,0 +1,81 @@
+import { Modal, View, Text, TextInput, TouchableOpacity } from 'react-native'
+import styles from './styles'
+import { useEffect, useState } from 'react'
+
+export default function TaskModal({ visible, onClose, onSave, taskToEdit }) {
+
+    const [form, setForm] = useState({
+        title: '',
+        description: '',
+        startDate: '',
+        endDate: '',
+        priority: ''
+    })
+
+    useEffect(() => {
+        if (taskToEdit) {
+            setForm(taskToEdit)
+        } else {
+            setForm({
+                title: '',
+                description: '',
+                startDate: '',
+                endDate: '',
+                priority: ''
+            })
+        }
+    }, [taskToEdit, visible])
+
+    return (
+        <Modal visible={visible} animationType='slide'>
+            <View style={styles.modalBody}>
+                <Text style={styles.label}>
+                    {taskToEdit ? 'Editar tarefa' : 'Nova tarefa'}
+                </Text>
+
+                <TextInput
+                    style={styles.input}
+                    placeholder='Título'
+                    value={form.title}
+                    onChangeText={v => setForm({ ...form, title: v })}
+                />
+
+                <TextInput
+                    style={styles.input}
+                    placeholder='Descrição'
+                    value={form.description}
+                    onChangeText={v => setForm({ ...form, description: v })}
+                />
+
+                <TextInput
+                    style={styles.input}
+                    placeholder='Data de início DD/MM'
+                    value={form.startDate}
+                    onChangeText={v => setForm({ ...form, startDate: v })}
+                />
+
+                <TextInput
+                    style={styles.input}
+                    placeholder='Data de término DD/MM'
+                    value={form.endDate}
+                    onChangeText={v => setForm({ ...form, endDate: v })}
+                />
+
+                <TextInput
+                    style={styles.input}
+                    placeholder='Prioridade: Alta, Média, Baixa'
+                    value={form.priority}
+                    onChangeText={v => setForm({ ...form, priority: v })}
+                />
+
+                <TouchableOpacity style={styles.btnSave} onPress={() => onSave(form)}>
+                    <Text style={styles.btnText}>Salvar Tarefa</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity onPress={onClose}>
+                    <Text style={styles.btnCancelar}>Cancelar</Text>
+                </TouchableOpacity>
+            </View>
+        </Modal>
+    )
+}
