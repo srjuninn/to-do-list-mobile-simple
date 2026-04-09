3f9dbd7ace24ae95ba547b98951ad5150ea64949
srjuninn <ryanferreiradasilva2006@gmail.com>
2026-04-09 11:05:43 -0300

    style: add task modal

---
+++ src/components/TaskModal/styles.js
@@ -0,0 +1,40 @@
+import { StyleSheet } from "react-native";
+import { theme } from '../../styles/global'
+
+export const styles = StyleSheet.create({
+    modalBody: {
+        flex: 1,
+        backgroundColor: theme.colors.background,
+        padding: 25,
+        paddingTop: 50
+    },
+    label: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginBottom: 20
+    },
+    input: {
+        backgroundColor: '#fff',
+        padding: 15,
+        borderRadius: 10,
+        marginBottom: 10,
+        borderWidth: 1,   // corrigido
+        borderColor: '#ddd'
+    },
+    btnSave: {
+        backgroundColor: theme.colors.primary,
+        padding: 15,
+        borderRadius: 10,
+        alignItems: 'center',
+        marginTop: 10,
+    },
+    btnCancelar: {
+        textAlign: 'center',
+        marginTop: 20,
+        color: '#f00'
+    },
+    btnText: {
+        color: '#fff',
+        fontWeight: 'bold'
+    }
+})
